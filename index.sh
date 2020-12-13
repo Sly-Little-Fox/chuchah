28bef6630f9438a8a45c02a66e79669eb344e5bd
Sly-Little-Fox <69681150+Sly-Little-Fox@users.noreply.github.com>
2020-12-13 16:10:12 +0300

    Update index.sh

--- index.sh
+++ index.sh
@@ -4,6 +4,6 @@ echo "Welcome to the chuchah, http ssh client!"
 while true
 do
 read -p "Enter your command here: "
-curl -X POST -H 'Content-type: text/plain' -d "$REPLY" 'https://TechnicalFlimsyNasm.mzfilin.repl.co'
+curl -X POST -H 'Content-type: text/plain' -d '$REPLY' 'your server' # I am lazy so I didn't make some input for it -_-
 clear
 done
